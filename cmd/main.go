7290b684be412c29eaeb7948f08b85806033c152
Akash <alfaxgem@gmail.com>
2026-09-21 15:08:49 +0530

    starting point with input loop and help commands skeleton

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,49 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"strings"
+
+	"github.com/chzyer/readline"
 )
 
-func main(){
-	log.Println("Assignment for Osto")
+func main() {
+	//log.Println("Assignment for Osto")
+
+	rl, err := readline.NewEx(&readline.Config{
+		Prompt:          "> ",
+		InterruptPrompt: "^C",
+		EOFPrompt:       "exit",
+	})
+	if err != nil {
+		log.Fatal("Error geting readline module")
+	}
+	defer rl.Close()
+
+	for {
+		line, err := rl.Readline()
+		if err != nil {
+			break
+		}
+		inputs := strings.Split(line, " ")
+
+		switch {
+		case inputs[0] == "/exit":
+			log.Println("exited")
+		case inputs[0] == "/help":
+			log.Println("asked for help")
+		case inputs[0] == "/login":
+			log.Println("asked for login")
+		case inputs[0] == "/register":
+			log.Println("asked for register")
+		default: 
+			fmt.Println("usage:	 /command {options}")
+			fmt.Println("commands:")
+			fmt.Println("	 /help")
+			fmt.Println("	 /register")
+			fmt.Println("	 /login")
+			fmt.Println("	 /exit")
+		}
+	}
 }
